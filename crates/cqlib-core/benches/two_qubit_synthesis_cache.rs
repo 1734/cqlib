bdfe31b52ef61c7100bc2237e9534bd94de41604
健健 <jianjian001@outlook.com>
2026-07-18 17:57:48 +0800

    chore(cqlib-core): add missing copyright headers to 8 source files

--- crates/cqlib-core/benches/two_qubit_synthesis_cache.rs
+++ crates/cqlib-core/benches/two_qubit_synthesis_cache.rs
@@ -1,3 +1,15 @@
+// This code is part of Cqlib.
+//
+// (C) Copyright China Telecom Quantum Group 2026
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE.txt file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
 use cqlib_core::circuit::{Circuit, Qubit, StandardGate, UnitaryGate};
 use cqlib_core::compile::transform::decompose::unitary::{
     UnitaryDecomposeConfig, decompose_unitaries_with_rule_stats,
